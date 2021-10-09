02315d418d4b3b0d0d914379fbddde8ad4860b81
Maria Javadyan <maria.javadyan02@gmail.com>
2021-10-10 00:41:12 +0400

    Move Components into separate files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
+import React from 'react';
+import ListWithAddItem from './components/ListWithAddItem';
 import SimpleList from './components/SimpleList';
+import ListWithRemoveItem from './components/ListWithRemoveItem';
+import ListWithComponents from './components/ListWithComponents';
 
-const myList = ["a", "b", "c", "d", "e", "f"]
 
 
-function App() {
-  return (
-    <div className="App">
-      <SimpleList list={myList} />
-    </div>
-  );
-}
+  const myList = ["a", "b", "c", "d", "e", "f","g"]
 
-export default App;
+
+  function App() {
+    return (
+      <div className="App">
+        <div>
+          <h3> Simple List </h3>
+          <SimpleList list={myList} />
+        </div>
+          <div>
+          <h3> List with Add Item Functionality </h3>  
+            <ListWithAddItem/>
+          </div>
+          <div>
+              <h3>List With Remove Item</h3>
+              <ListWithRemoveItem/>
+          </div>
+          <div>
+              <h3>List with Components</h3>
+              <ListWithComponents/>
+          </div>
+      </div>
+    );
+  }
+  export default App;
+
+
---
+++ src/components/ListWithComponents.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const list = [
+    {
+      id: 'a',
+      firstname: 'Robin',
+      lastname: 'Wieruch',
+      year: 1988,
+    },
+    {
+      id: 'b',
+      firstname: 'Dave',
+      lastname: 'Davidds',
+      year: 1990,
+    },
+  ];
+   
+  const ListWithComponents = () => <List list={list} />;
+   
+  const List = ({ list }) => (
+    <ul>
+      {list.map(item => (
+        <ListItem key={item.id} item={item} />
+      ))}
+    </ul>
+  );
+   
+  const ListItem = ({ item }) => (
+    <li>
+      <div>{item.id}</div>
+      <div>{item.firstname}</div>
+      <div>{item.lastname}</div>
+      <div>{item.year}</div>
+    </li>
+  );
+  export default ListWithComponents;
